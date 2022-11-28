d75b732804e60a601ba08ee77e1a83ca4972f809
Pierre souza <pas@lionx.com.br>
2022-11-28 15:51:44 -0300

    chore: apply validation in fields

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,15 @@ interface IFormInputs {
   WorkYears: number;
   email: string;
   select: string;
+  myRadio: [];
 }
-
 const schema = yup
   .object({
     User: yup.string().required("O nome é obrigatório").required("O nome é obrigatório !"),
-    WorkYears: yup.number().positive().required("insira seu tempo de experiência !"),
+    WorkYears: yup.number().positive().integer().required("insira seu tempo de experiência !"),
     email: yup.string().email("Digite um e-mail válido").required("O e-mail é obigatório !"),
     select: yup.string().required("selecione uma opção !"),
+    myRadio: yup.array().min(1, "Selecione ao menos uma opção"),
   })
   .required();
 
@@ -25,8 +26,18 @@ export function App() {
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm<IFormInputs>({ resolver: yupResolver(schema) });
+
+  const data = [
+    { text: "ReactJs", value: "ReactJs", id: 1 },
+    { text: "Javascript", value: "Javascript", id: 2 },
+    { text: "Typescript", value: "Typescript", id: 3 },
+    { text: "CSS", value: "CSS", id: 4 },
+    { text: "Sass", value: "Sass", id: 5 },
+    { text: "Less", value: "Less", id: 6 },
+  ];
 
   function Onsubmit(userData: any) {
     console.log(userData);
@@ -34,26 +45,37 @@ export function App() {
   return (
     <>
       <div className="App">
-        <img src={logo} height="80" alt="" />
+        <div className="logo">
+          <p>Dev Forms</p>
+          <img src={logo} height="80" alt="" />
+        </div>
         <div className="formulario">
           <form onSubmit={handleSubmit(Onsubmit)}>
             <label className="person-info">
-              Nome
+              <p>Nome</p>
               <input type="text" minLength={4} {...register("User")} />
               <span className="errors">{errors.User?.message}</span>
             </label>
             <label className="person-info">
-              Anos na área?
-              <input type="number" {...register("User")} />
-              <span className="errors">{errors.User?.message}</span>
+              <p>Anos na área?</p>
+              <input
+                type="number"
+                {...register("WorkYears", {
+                  setValueAs: (value) => {
+                    const maybeNumber = parseInt(value, 10);
+                    return isNaN(maybeNumber) ? 0 : maybeNumber;
+                  },
+                })}
+              />
+              <span className="errors">{errors.WorkYears?.message}</span>
             </label>
             <label className="person-info">
-              E-mail
+              <p>E-mail</p>
               <input type="text" {...register("email")} />
               <span className="errors">{errors.email?.message}</span>
             </label>
             <div className="office">
-              <span className="office-title">Qual o seu cargo ?</span>
+              <p className="person-info">Qual o seu cargo ?</p>
               <select {...register("select")}>
                 <option value=""></option>
                 <option value="Junior">Junior</option>
@@ -63,8 +85,21 @@ export function App() {
               </select>
               <span className="errors">{errors.select?.message}</span>
             </div>
+            <div className="multi-options">
+              <p className="text-options">Quais linguagens você conhece ?</p>
+              {data.map(({ id, text, value }) => (
+                <div key={id} {...register("myRadio")}>
+                  <p>{text}</p>
+                  <input type="checkbox" value={value} />
+                </div>
+              ))}
+              <span className="errors">{errors.myRadio?.message}</span>
+            </div>
             <div className="area-button">
               <button type="submit">Enviar Dados</button>
+              <button type="submit" onClick={() => reset()}>
+                Limpar Dados
+              </button>
             </div>
           </form>
         </div>
